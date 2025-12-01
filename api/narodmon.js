8b1fa8075b8bcbac19520daf18983cd2f97e2a62
pipapsix <32680608+pipapsix@users.noreply.github.com>
2025-12-01 18:40:33 +0300

    initial commit

---
+++ api/narodmon.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  const query = req.url.split('?')[1] || '';
+  const url = 'https://narodmon.ru/api/?' + query;
+  
+  try {
+    const response = await fetch(url);
+    const data = await response.text();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send('Proxy error');
+  }
+}
