369451c670dfb58d0e8fc27980e1ad52c2dadc9a
pipapsix <32680608+pipapsix@users.noreply.github.com>
2025-12-01 18:49:08 +0300

    Add headers to fetch request in narodmon.js

--- api/narodmon.js
+++ api/narodmon.js
@@ -3,8 +3,14 @@ export default async function handler(req, res) {
   const url = 'https://narodmon.ru/api/?' + query;
   
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      headers: {
+        'X-Real-IP': '85.26.248.1',       // любой российский IP
+        'X-Forwarded-For': '85.26.248.1'
+      }
+    });
     const data = await response.text();
+    
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
     res.status(200).send(data);
